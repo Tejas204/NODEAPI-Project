acd74904fbb36d8d25f681ef8fccaffdb117b4c3
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2023-11-19 22:19:20 +0530

    Added API call chaining

--- models/user.js
+++ models/user.js
@@ -3,8 +3,18 @@ import mongoose from "mongoose";
 // Database Schema
 const schema  = new mongoose.Schema({
     name: String,
-    email: String,
-    password: String
+    email: {
+        type: String,
+        unique: true,
+    },
+    password: {
+        type: String,
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    }
 });
 
 export const User = mongoose.model("User", schema);
