3073141b2367685674202389beaabeb9c0094720
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2023-11-19 22:15:35 +0530

    Added API call chaining

--- routes/user.js
+++ routes/user.js
@@ -10,15 +10,11 @@ router.get("/all", getAllUsers);
 // API: Create new user
 router.post("/new", createNewUser);
 
-// API: delete an user
-router.delete("/delete/:id", deleteUser);
-
-// Update user
-router.put("/userid/:id", updateUser);
-
 // API: get user by user id
-// Dynamic route should ideally be placed at the end
-router.get("/userid/:id", getUserbyUserId);
+// API: Update user
+// API: delete an user
 
+// If base route is the same, we can chain the calls together as below
+router.route("/userid/:id").get(getUserbyUserId).put(updateUser).delete(deleteUser);
 
 export default router;
